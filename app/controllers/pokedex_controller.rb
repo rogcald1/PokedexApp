f572768d9f80b813d7016ccc78760ec8d02291d8
rogcald1 <roger@justworks.com>
2021-03-16 11:39:07 -0400

    worked more on the search function, got the pokemon name to appear from the API and an error message to pop up if the name is invalid. also added search view and worked on controller

--- app/controllers/pokedex_controller.rb
+++ app/controllers/pokedex_controller.rb
@@ -5,22 +5,21 @@ class PokedexController < ApplicationController
   end
 
   def search
-    pokemons = find_pokemon(params[:pokemon])
 
-    unless pokemons
+    ##initial test for error
+    test_res = Excon.get("https://pokeapi.co/api/v2/pokemon/#{params[:pokemon]}")
+
+    if test_res.data[:body] == 'Not Found'
       flash[:alert] = "pokemon not found :/"
       return render action: :index
+    else
+      pokemons = find_pokemon(params[:pokemon]) 
     end
 
-    private
+    @pokemon_name = pokemons.name
+  end
 
-    def find_pokemon(pokemon_choice)
-      response = PokeApi.get(pokemon: "#{pokemon_choice}") 
-
-      return nil if response.status != 200
-
-      JSON.parse(response.body)
-    end
-
+  def find_pokemon(pokemon_choice)
+    response = PokeApi.get(pokemon: "#{pokemon_choice}")
   end
 end
