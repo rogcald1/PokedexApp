c31a36fd1421159144e53f0e939bc2eef6de2e30
rogcald1 <roger@justworks.com>
2021-03-29 18:55:19 -0400

    Minor updates to the look, added the description, added in four sections for height, weight, abilities, and genus, and started on height (needed conversion) and abilities

--- app/controllers/pokedex_controller.rb
+++ app/controllers/pokedex_controller.rb
@@ -15,13 +15,39 @@ class PokedexController < ApplicationController
     else
     ##if initial test passes, then use PokeApi and grab info
       pokemons = find_pokemon(params[:pokemon].downcase) 
+      species = pokemons.species.get
     end
+
+    @pokemon_desc = ''
+
+    species.flavor_text_entries.each {|k|
+      @pokemon_desc = k.flavor_text if k.language.name == 'en' && k.version.name == 'red'
+    }
 
     @pokemon_name = pokemons.name.capitalize
     @pokemon_id = pokemons.id
+    
+    height_arr = height_converter(pokemons.height)
+    @ft = height_arr[0]
+    @inches = height_arr[1]
+
+    @abilities = []
+
+    pokemons.abilities.each {|k|
+      @abilities << k.ability.name.gsub('-',' ').split.map(&:capitalize).join(' ')
+    }
   end
 
   def find_pokemon(pokemon_choice)
     response = PokeApi.get(pokemon: "#{pokemon_choice}")
+  end
+
+  def height_converter(height)
+    final = []
+    first_conv = (height*3.94)/12
+    second_conv = first_conv.to_s.split('.')
+    final << second_conv[0]
+    final << second_conv[1][0..1]
+    final
   end
 end
